21bac88b3d6870ce774c0f6166580127928a5703
Dwiki Nugroho <dwikinugroho67@gmail.com>
2026-09-02 09:03:54 +0700

    feat(root): register Reel1VideoEnhanced composition in RemotionRoot

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Composition } from "remotion";
 import { Reel1Video, reel1TotalFrames } from "./Reel1Video";
+import { Reel1VideoEnhanced } from "./Reel1VideoEnhanced";
 
 export const RemotionRoot: React.FC = () => {
   return (
     <>
-      {/* Reel 1 Video */}
+      {/* Reel 1 Video (Original) */}
       <Composition
         id="Reel1Video"
         component={Reel1Video}
@@ -14,6 +15,17 @@ export const RemotionRoot: React.FC = () => {
         width={1080}
         height={1920}
       />
+
+      {/* Reel 1 Video (Enhanced - Modern Kinetic Typography & Cinematic Visuals) */}
+      <Composition
+        id="Reel1VideoEnhanced"
+        component={Reel1VideoEnhanced}
+        durationInFrames={reel1TotalFrames}
+        fps={30}
+        width={1080}
+        height={1920}
+      />
     </>
   );
 };
+
